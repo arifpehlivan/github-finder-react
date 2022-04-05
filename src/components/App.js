198d26be05c966b1f94f294ac46ad762fc1e3b9b
arifpehlivan <arifpehlivanapap@gmail.com>
2022-04-06 02:54:47 +0300

    axios

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,25 @@
 import React, { Component } from 'react'
 import Navbar from './Navbar'
 import Users from './Users'
+import axios from "axios"
 
 export class App extends Component {
+  constructor(props){
+    super(props);
+    this.state={
+      users:[]
+    }
+  }
+  componentDidMount(){
+    axios
+    .get("https://api.github.com/users")
+    .then(res=>this.setState({users:res.data}));
+  }
   render() {
     return (
       <>
         <Navbar title="Github Finder" icon="fa-brands fa-github"/>
-        <Users/>
+        <Users users={this.state.users}/>
       </>
       
     )
--- src/components/Users.js
+++ src/components/Users.js
@@ -2,36 +2,11 @@ import React, { Component } from 'react'
 import User from './User'
 
 class Users extends Component {
-    constructor(props){
-        super(props);
-        this.state={
-            users:[
-                {
-                    id:"1",
-                    login: "arifpehlivan",
-                    avatar_url: "https://avatars.githubusercontent.com/u/85321673?v=4",
-                    html_url: "https://github.com/arifpehlivan",
-                },
-                {
-                    id:"2",
-                    login: "arifpehlivan",
-                    avatar_url: "https://avatars.githubusercontent.com/u/85321673?v=4",
-                    html_url: "https://github.com/arifpehlivan",
-                },
-                {
-                    id:"3",
-                    login: "arifpehlivan",
-                    avatar_url: "https://avatars.githubusercontent.com/u/85321673?v=4",
-                    html_url: "https://github.com/arifpehlivan",
-                },
-            ]
-        }
-    }
   render() {
     return (
       <div className='container mt-3'>
           <div className="row">
-            {this.state.users.map(user=>(
+            {this.props.users.map(user=>(
                 <User key={user.id} user={user}/>
             ))}
           </div>
