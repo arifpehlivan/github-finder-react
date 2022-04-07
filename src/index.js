2143cb18b348c37892e727f937eeca7ff9804435
arifpehlivan <arifpehlivanapap@gmail.com>
2022-04-08 01:50:22 +0300

    route link

--- src/index.js
+++ src/index.js
@@ -2,10 +2,28 @@ import React from "react";
 import { createRoot } from 'react-dom/client';
 // import App from "./components/App";
 import "./styles/main.scss"
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes, NavLink } from "react-router-dom";
 
 const container = document.getElementById('app');
 const root = createRoot(container);
+
+const Nav = () =>{
+    return(
+        <nav>
+            <NavLink exact="true" to="/" activeclassname="active">Home</NavLink>
+            <NavLink to="/contact" activeclassname="active">Contact</NavLink>
+            <NavLink to="/products" activeclassname="active">Products</NavLink>
+        </nav>
+
+    // <nav>
+    //     <Link to="/">Home</Link>
+    //     <Link to="/contact">Contact</Link>
+    //     <Link to="/products">Products</Link>
+    // </nav>
+
+    
+    )
+}
 
 const HomePage = () => {
     return (
@@ -41,6 +59,7 @@ const NotFound = () => {
 
 const routes=(
     <BrowserRouter>
+        <Nav/>
         <Routes>
             <Route path="/" element={<HomePage />}></Route>
             <Route path="/contact" element={<ContactPage />}></Route>
