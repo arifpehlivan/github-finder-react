45f1df7b9d4880aacaae7dff338c8bfb36a3567d
arifpehlivan <arifpehlivanapap@gmail.com>
2022-04-08 02:57:42 +0300

    finish

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 // import React from "react";
 import { createRoot } from 'react-dom/client';
-// import App from "./components/App";
+import App from "./components/App";
 import "./styles/main.scss";
-import AppRouter from './routers/AppRouter';
+// import AppRouter from './routers/AppRouter';
 
 
 const container = document.getElementById('app');
@@ -10,5 +10,5 @@ const root = createRoot(container);
 
 
 
-root.render(<AppRouter/>);
+root.render(<App/>);
 
